4b36b5cb40e200799a86d20555dad1220635719c
rafmasloman <raflymasloman20@gmail.com>
2023-12-14 19:05:32 +0800

    feat: create navbar component

---
+++ src/common/components/molecul/navbar/navbar.component.tsx
@@ -0,0 +1,12 @@
+import { Group, Image } from '@mantine/core';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Image src={''} alt="Logo Image" />
+      <Group></Group>
+    </div>
+  );
+};
+
+export default Navbar;
